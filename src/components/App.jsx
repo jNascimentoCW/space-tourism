a618f1cd6f61b391e72c66e44672bbc4b0b6dcb5
Jhonatan Nascimento <j.nascimento3138@gmail.com>
2025-12-16 15:40:46 -0300

    feat: creating crew component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
+
 import "./App.css";
 import Destination from "./Destination/Destination";
 import Home from "./Home/Home";
 import Navbar from "./Navbar/Navbar";
 
 function App() {
+  const [page, setPage] = useState("main");
   return (
     <>
       <Navbar />
-      {/* <Home /> */}
-      <Destination />
+      <Home />
+      {/* <Destination /> */}
     </>
   );
 }
